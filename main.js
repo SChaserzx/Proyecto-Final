b59468b3c57a69373239abee01070bcd59d4b079
SChaserzx <129403104+SChaserzx@users.noreply.github.com>
2023-06-13 22:37:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var getData = function(){
+    var name= document.getElementById("nombre").value;
+    var lastName= document.getElementById("apellido").value;
+    var tel= document.getElementById("telefono").value;
+    var email= document.getElementById("email").value;
+    var mensaje= document.getElementById("mensaje").value;
+    console.log("nombre");
+    
+}
